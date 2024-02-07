f9d36aa53c3214c8470d99bb4c553ead738efcf2
Habiboyewo <habiboyewo98@gmail.com>
2024-02-07 16:30:53 +0100

    first commit

---
+++ src/adminContext.jsx
@@ -0,0 +1,43 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useGlobalContext } from "./Hooks/useGlobalContext";
+import axios from "axios";
+
+const AdminContext = createContext()
+export const UseAdminContext = () => useContext(AdminContext)
+
+
+const AdminProvider = ({ children }) => {
+    const { BASE_URL } = useGlobalContext()
+    const [properties, setProperties] = useState([])
+    const [isLoading, setIsLoading] = useState([])
+    const [type, setType] = useState("")
+    const [location, setLocation] = useState("")
+
+    const updateType = (e) => {
+        console.log(e.target.value);
+        setType(e.target.value)
+    }
+
+    const url = `${BASE_URL}/property?type=${type}&location=${location}`
+    const getProperties = async () => {
+        try {
+            setIsLoading(true)
+            const { data } = await axios(url)
+            console.log(data);
+            setProperties(data.properties)
+            setIsLoading(false)
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        getProperties()
+    }, [type, location])
+
+    return <AdminContext.Provider value={{ isLoading, properties, type, updateType, location, setLocation }}>
+        {children}
+    </AdminContext.Provider>
+}
+
+export default AdminProvider
